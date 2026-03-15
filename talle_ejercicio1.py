125d30a60bffdd9e1abe58b12ec9263922a0d9d3
juancalle-17 <juan.calle1@utp.edu.co>
2026-03-15 15:01:50 -0500

    Add loops for squares, multiples of 10, and powers of 2
    codigo diseñado para calcular los cuadrados menores al limite que definimos, los valores positivos que son divisibles por 10 y menores que el limite y las potencias de 2 menores que el limite.

---
+++ talle_ejercicio1.py
@@ -0,0 +1,23 @@
+print("bucle de valores cuadrados menores al limite")
+limite=int(input("ingrese el limite del bucle"))
+inicial=0
+
+while inicial*inicial<limite:
+    print(inicial * inicial)
+    inicial = inicial + 1
+
+print("valores positivos que son divisibles por 10 y menores que el limite")
+limite=int(input("ingrese el limite del bucle"))
+inicial=0
+
+while inicial <limite:
+    print(inicial)
+    inicial = inicial +10
+
+print("potencias de 2 menores al limite")
+limite=int(input("ingrese el limite del bucle"))
+inicial=0
+
+while 2*inicial <limite:
+    print(2*inicial)
+    inicial = inicial + 1
